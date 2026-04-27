f25c462f9a117b7ca152e7e7e3d36879a60eefd6
jstruessel <jstruessel@callutheran.edu>
2026-04-27 08:20:46 -0700

    Update ScatterPlotPlotly.py

--- ScatterPlotPlotly.py
+++ ScatterPlotPlotly.py
@@ -3,9 +3,11 @@ import plotly.express as px
 
 df = pd.read_csv("https://raw.githubusercontent.com/jstruessel/LAparking/refs/heads/main/Los_Angeles_International_Airport__LAX__-_Parking_Lots_Current_Status_20250122.csv")
 print(df.columns)
-fig = px.scatter(df, x="TotalParkingSpaces", y="Occupied", color="FullCapacity", hover_data=["Long","Lat"])
-fig.show()
 
-fig2 = px.line(df, x="TotalParkingSpaces", y="Occupied", color="FullCapacity", hover_data=["Long","Lat"])
-fig2.show()
+fig = px.scatter(df,
+                 x="TotalParkingSpaces",
+                 y="Occupied",
+                 color="FullCapacity",
+                 hover_data=["Long","Lat"])
+fig.show()
 
