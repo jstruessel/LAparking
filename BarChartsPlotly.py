b9478b029a6f468b6131226ec3b8b9493e877c16
jstruessel <jstruessel@callutheran.edu>
2026-04-27 08:19:37 -0700

    Update BarChartsPlotly.py

--- BarChartsPlotly.py
+++ BarChartsPlotly.py
@@ -4,5 +4,10 @@ import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/jstruessel/LAparking/refs/heads/main/Los_Angeles_International_Airport__LAX__-_Parking_Lots_Current_Status_20250122.csv")
 print(df.columns)
 
-fig = px.bar(df, x='LotDescription', y='Occupied', title='Occupied Spaces by Lot', color= "FullCapacity", hover_data=["Long","Lat"])
+fig = px.bar(df,
+             x='LotDescription',
+             y='Occupied',
+             title='Occupied Spaces by Lot',
+             color= "FullCapacity",
+             hover_data=["Long","Lat"])
 fig.show()
